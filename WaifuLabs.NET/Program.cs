7743381240c0a48b05281848289253bb564dcfae
Conner Starkel <csmediaseattle@gmail.com>
2020-11-25 13:53:01 -0800

    Add project files.

---
+++ WaifuLabs.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WaifuLabs.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
